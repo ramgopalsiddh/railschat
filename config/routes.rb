8c44516d32328c247dbe031148901a9cb8b22540
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-02-14 14:19:21 +0530

    Day 21 Add message partial and refactor some code

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   
-  root 'chatroom#home'
+  root 'chatroom#index'
   get 'login', to: 'sessions#new'
 
 end
