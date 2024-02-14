eda7ed21364742a3798a0545db815fcb358c7cab
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-02-15 01:06:04 +0530

    Day 22 Display message using partial(show message without refresh )

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -10,6 +10,7 @@ consumer.subscriptions.create("ChatroomChannel", {
   },
 
   received(data) {
+    $('#message-container').append(data.mod_message)
     // Called when there's incoming data on the websocket for this channel
   }
 });
